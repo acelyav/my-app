28838fa218a974cd8c84658ded389938d905d9c2
Acelya Vuluvan <acelyavuluvan@gmail.com>
2021-07-08 16:31:47 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import "./App.css";
 import pokemon from "./pokemon.json";
 
-const PokemonRow = ({ pokemon }) => (
-  <tr
-    // key={[pokemon.id, pokemon.name.english].join(':')} create unique id
-    key={pokemon.id}
-  >
+const PokemonRow = ({ pokemon, onSelect }) => (
+  <tr>
     <td>{pokemon.name.english}</td>
     <td>{pokemon.type.join(", ")}</td>
+    <td>
+      <button onClick={() => onSelect(pokemon)}>Select!</button>
+    </td>
   </tr>
 );
 
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  }),
+};
+
 function App() {
+  const [filter, filterSet] = React.useState("");
+  const [selectedItem, selectedItemSet] = React.useState(null);
+
   return (
     <div
       style={{
@@ -22,17 +36,46 @@ function App() {
       }}
     >
       <h1 className="title">Pokemon Search</h1>
-      <table width="100%">
-        <thead>
-          <th>Name</th>
-          <th>Type</th>
-        </thead>
-        <tbody>
-          {pokemon.slice(0, 20).map((pokemon) => (
-            <PokemonRow pokemon={pokemon} key={pokemon.id} />
-          ))}
-        </tbody>
-      </table>
+      <div>
+        <input value={filter} onChange={(evn) => filterSet(evn.target.value)} />
+      </div>
+      <div
+        style={{
+          display: "grid",
+        }}
+      >
+        <table width="100%">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pokemon
+              .filter((pokemon) =>
+                pokemon.name.english
+                  .toLowerCase()
+                  .includes(filter.toLowerCase())
+              )
+              .slice(0, 20)
+              .map((pokemon) => (
+                <PokemonRow
+                  pokemon={pokemon}
+                  key={pokemon.id}
+                  onSelect={(pokemon) => selectedItemSet(pokemon)}
+                />
+                /* Create Unique ID
+                key={[pokemon.id, pokemon.name.english].join(':')} */
+              ))}
+          </tbody>
+        </table>
+      </div>
+      {selectedItem && (
+        <div>
+          <h1>{selectedItem.name.english}</h1>
+        </div>
+      )}
     </div>
   );
 }
